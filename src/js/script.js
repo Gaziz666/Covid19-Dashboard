86aa270d003afba0c63ac2367638f4b06d665d26
Gaziz Tuleukulov <gaziz.cyprus@gmail.com>
2020-12-09 18:30:35 +0300

    feat: add standart app

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,26 @@
 import '../css/style.css';
+import ListModel from './models/listModel'
+import ListController from './controller/ListController';
+import ListView from './views/ListViewer'
 
 require.context('./../assets/img', true, /\.(png|svg|jpg|gif)$/);
 require.context('./../assets/audio', true, /\.wav$/);
 
 const div = document.createElement('div');
-div.innerHTML = 'start';
+div.innerHTML = `Your favourite JavaScript technologies:<br>
+<select id="list" size="10" style="width: 17em"></select><br>
+<button id="plusBtn">  +  </button>
+<button id="minusBtn">  -  </button>`;
 document.body.append(div);
+
+window.addEventListener('load', () => {
+  const model = new ListModel(['node.js', 'react']),
+    view = new ListView(model, {
+      'list' : document.getElementById('list'),
+      'addButton' : document.getElementById('plusBtn'), 
+      'delButton' : document.getElementById('minusBtn')
+    }),
+    controller = new ListController(model, view);
+
+  view.show();
+});
